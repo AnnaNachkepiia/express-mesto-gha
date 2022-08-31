5ee98109692ec96e647b3212f0112c6909990e6f
Anna Nachkepiia <admin@MacBook-Pro-AnnaNachkepiia.local>
2022-08-31 22:25:59 +0300

    13

--- app.js
+++ app.js
@@ -17,55 +17,31 @@ mongoose.connect("mongodb://localhost:27017/mestodb", {});
 app.use(express.json());
 // app.use(bodyParser.json());
 
-// app.post(
-//   "/signup",
-//   celebrate({
-//     body: Joi.object().keys({
-//       name: Joi.string().min(2).max(30),
-//       about: Joi.string().min(2).max(30),
-//       avatar: Joi.string().regex(
-//         /https?:\/\/(www\.)?([-a-zA-Z0-9()@:%_+.~#?&//=]*)/,
-//       ),
-//       email: Joi.string().required().email(),
-//       password: Joi.string().required().min(8),
-//     }),
-//   }),
-//   createUser,
-// );
-// app.post(
-//   "/signin",
-//   celebrate({
-//     body: Joi.object().keys({
-//       email: Joi.string().required().email(),
-//       password: Joi.string().required().min(8),
-//     }),
-//   }),
-//   login,
-// );
-
-app.post(
-  "/signup",
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().regex(/^https?:\/\/(?:www\.)?([\w-]+\.)+\/?\S*$/),
-    }),
-  }),
-  createUser,
-);
-
 app.post(
   "/signin",
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
-      password: Joi.string().required(),
+      password: Joi.string().required().min(8),
     }),
   }),
   login,
+);
+
+app.post(
+  "/signup",
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().regex(
+        /https?:\/\/(www\.)?([-a-zA-Z0-9()@:%_+.~#?&//=]*)/,
+      ),
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(8),
+    }),
+  }),
+  createUser,
 );
 
 app.use(auth);
