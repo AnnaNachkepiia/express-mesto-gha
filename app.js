6bb6f995c65c4ff4f30f3af5a0b9798ec3ee02dc
Anna Nachkepiia <admin@MacBook-Pro-AnnaNachkepiia.local>
2022-09-03 09:32:03 +0300

    add helmet

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 const { errors } = require('celebrate');
 const errorsType = require('./middlewares/errorsType');
 const router = require('./routes');
@@ -16,7 +17,7 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb', {});
 
 app.use(express.json());
-
+app.use(helmet());
 app.use(router);
 
 app.use(errors());
