3bc49b271c9e56ae9761c2e5275cdd00aa1c660b
Anna Nachkepiia <admin@MacBook-Pro-AnnaNachkepiia.local>
2022-09-08 14:18:58 +0300

    15 project 3000 port

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const errorsType = require('./middlewares/errorsType');
 const router = require('./routes');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 // const cors = require('./middlewares/cors');
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 const options = {
   origin: [
-    // 'http://localhost:3000',
+    'http://localhost:3001',
     'http://localhost:3000',
     // 'http://localhost:порт',
     // 'https://ВАШ ДОМЕЙН С ДОКУМЕНТА',
