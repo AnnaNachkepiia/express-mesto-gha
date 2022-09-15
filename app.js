a88274920ff9f6061f2fc6f46c445bbaae0536d4
Anna Nachkepiia <admin@MacBook-Pro-AnnaNachkepiia.local>
2022-09-15 19:40:07 +0300

    add cookieParser

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const errorsType = require('./middlewares/errorsType');
 const router = require('./routes');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const cors = require('./middlewares/cors');
+const cookieParser = require('cookie-parser');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -27,9 +28,8 @@ const app = express();
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://localhost:27017/mestodb', {});
 // app.use(cors(options));
-
 app.use(express.json());
-
+app.use(cookieParser);
 app.use(helmet());
 app.use(requestLogger);
 
